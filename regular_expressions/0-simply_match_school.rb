5cf0c234e78847b11bcd1f634d6c7de2754dbf0b
Erica015 <e.ishimwe2@alustudent.com>
2025-01-09 23:58:40 -0800

    0

--- regular_expressions/0-simply_match_school.rb
+++ regular_expressions/0-simply_match_school.rb
@@ -1,14 +1,2 @@
 #!/usr/bin/env ruby
 puts ARGV[0].scan(/School/).join
-# Single match
-./0-simply_match_school.rb School | cat -e
-School$
-
-# Multiple matches
-./0-simply_match_school.rb "Best School School" | cat -e
-SchoolSchool$
-
-# No match
-./0-simply_match_school.rb "Grace Hopper" | cat -e
-$
-
